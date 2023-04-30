978250d829ee56119f58902dbf6f7be17a74ce6e
richard wetterauer <rhwette2022@gmail.com>
2023-04-30 16:08:17 -0500

    header, intro, footer responsive

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Grid, GridItem } from '@chakra-ui/react';
+import { Box, Heading, Text } from '@chakra-ui/react';
+
 import Header from './components/Header.jsx';
-import Intro from './components/Intro';
-import Problems from './components/Problems';
-import Techniques from './components/Techniques';
+import Intro from './components/Intro/Intro.jsx';
+import Problems from './components/Problems/Problems.jsx';
+import Techniques from './components/Techniques/Techniques.jsx';
 import Ted from './components/Ted';
 import Oakley from './components/Oakley';
 import Feynman from './components/Feynman';
@@ -12,74 +14,80 @@ import Kahan from './components/Kahan';
 import Principles from './components/Principles';
 import Resources from './components/Resources';
 import Footer from './components/Footer';
+// import './components/Page/page.css';
 
 function App() {
   return (
-    <>
+    <Box
+      w={['320px', '375px', '425px', '768px', '1024px', '1440px']}
+      // w="100%"
+      h="100%"
+      bg="red.100"
+    >
       <Grid
-        templateAreas={` "header header"
-                              "intro intro"
-                              "problems problems"
-                              "techniques techniques"
-                              "ted ted"
-                              "oakleyaside oakleyaside"
-                              "feynman feynman"
-                              "factsandfigs factsandfigs"
-                              "kahan kahan"
-                              "principles principles"
-                              "resources resources"
-                              "footer footer"
-                              `}
+        templateAreas={`  "header"
+                                "intro"
+                                "problems"
+                                "techniques"
+                                "ted"
+                                "oakleyaside"
+                                "feynman"
+                                "factsandfigs"
+                                "kahan"
+                                "principles"
+                                "resources"
+                                "footer"
+                                `}
       >
-        <GridItem area="header" bg="white">
-          <Header />
+        <GridItem area="header">
+          <Header></Header>
         </GridItem>
 
-        <GridItem area="intro" bg="white">
+        <GridItem area="intro">
           <Intro />
         </GridItem>
 
-        <GridItem area="problems" bg="white">
+        {/* <GridItem area="problems">
           <Problems />
-        </GridItem>
+        </GridItem> */}
 
-        <GridItem area="techniques" bg="white">
+        {/* <GridItem area="techniques">
           <Techniques />
-        </GridItem>
+        </GridItem> */}
 
-        <GridItem area="ted" bg="white">
+        {/* <GridItem area="ted">
           <Ted />
-        </GridItem>
+        </GridItem> */}
 
-        <GridItem area="oakleyaside" bg="orange">
+        {/* <GridItem area="oakleyaside" bg="orange">
           <Oakley />
-        </GridItem>
+        </GridItem> */}
 
-        <GridItem area="feynman" bg="white">
+        {/* <GridItem area="feynman">
           <Feynman />
-        </GridItem>
+        </GridItem> */}
 
-        <GridItem area="factsandfigs" bg="white">
+        {/* <GridItem area="factsandfigs">
           <FactsAndFigs />
-        </GridItem>
+        </GridItem> */}
 
-        <GridItem area="kahan" bg="gray">
+        {/* <GridItem area="kahan" bg="gray">
           <Kahan />
-        </GridItem>
+        </GridItem> */}
 
-        <GridItem area="principles" bg="black">
+        {/* <GridItem area="principles" bg="black">
           <Principles />
-        </GridItem>
+        </GridItem> */}
 
-        <GridItem area="resources" bg="#D3D3D3">
+        {/* <GridItem area="resources" bg="#D3D3D3">
           <Resources />
-        </GridItem>
+        </GridItem> */}
 
         <GridItem area="footer" bg="black">
           <Footer />
         </GridItem>
       </Grid>
-    </>
+    </Box>
   );
 }
 
